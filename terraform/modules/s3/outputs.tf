e657ee5a4135073b53d64c5b078e24406036a7a9
plusforum <103184903+plusforum@users.noreply.github.com>
2026-01-19 11:49:54 +0530

    Update outputs.tf

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,14 +1,7 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+output "bucket_name" {
+  value = aws_s3_bucket.data_bucket.bucket
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
-}
-
-output "instance_hostname" {
-  description = "Private DNS name of the EC2 instance"
-  value       = aws_instance.app_server.private_dns
+output "bucket_arn" {
+  value = aws_s3_bucket.data_bucket.arn
 }
